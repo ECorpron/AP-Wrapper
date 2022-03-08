0128fd2cb740c79a36c1371209ac94d51f731441
ECorpron <ecorpron@gmail.com>
2022-03-08 14:28:05 -0800

    adding documentation folder

--- examples/Example Script.py
+++ examples/Example Script.py
@@ -7,7 +7,10 @@ gameId = 1172470
 achRequest = f'http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid={gameId}'
 
 ach = requests.get(achRequest)
+
+print("Using the requests package to get game achievements \n")
 print(ach.text)
+print("\n")
 
 # Using the APIWrapper class
 getAch = {"getGameAch": achRequest}
@@ -15,8 +18,13 @@ getAch = {"getGameAch": achRequest}
 apiWrap = api.APIWrapper()
 apiWrap.updateGetRequests(getAch)
 
+print("Using the apiWrapper class to get game achievements \n")
 print(apiWrap.executeGet("getGameAch").text)
+print("\n")
 
 # Using the SteamworksWrapper class
 steamWrap = steamApi.SteamworksWrapper()
+
+print("Using the SteamworksWrapper class to get game achievements \n")
 print(steamWrap.getGameAchievements(gameId))
+print("\n")
