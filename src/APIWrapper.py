b16a1e62afcc0db8117d533667d7be8ec48a99e9
ECorpron <ecorpron@gmail.com>
2022-03-07 23:16:00 -0800

    added formatting for private variables

--- src/APIWrapper.py
+++ src/APIWrapper.py
@@ -1,67 +1,67 @@
 import requests
 
 class APIWrapper():
-    getRequests = {}
-    postRequests = {}
-    keys = {}
+    __getRequests = {}
+    __postRequests = {}
+    __keys = {}
     statusCode = None
 
     def __init__(self):
         pass
 
     def updateValue(self, dict):
-        self.keys.update(dict)
-        return keys
+        self.__keys.update(dict)
+        return self.__keys
 
     def getKeys(self):
-        return self.keys
+        return self.__keys
 
     def getKey(self, key):
-        return self.keys[key]
+        return self.__keys[key]
 
     def replaceKeys(self, newKeys):
-        self.keys = newKeys
+        self.__keys = newKeys
 
     def addKeys(self, dict):
-        self.keys.update(dict)
-        return self.keys
+        self.__keys.update(dict)
+        return self.__keys
 
     def deleteKey(self, key):
-        value = self.keys.pop(key)
+        value = self.__keys.pop(key)
         return value
 
     def deleteAllKeys(self):
-        self.keys.clear()
+        self.__keys.clear()
 
     def updateGetRequests(self, dict):
-        self.getRequests.update(dict)
-        return self.getRequests
+        self.__getRequests.update(dict)
+        return self.__getRequests
 
     def deleteGetRequest(self, key):
-        self.getRequests.remove(key)
-        return self.getRequests
+        self.__getRequests.remove(key)
+        return self.__getRequests
 
     def deleteAllGetRequests(self):
-        self.getRequests.clear()
+        self.__getRequests.clear()
 
     def updatePostRequests(self, dict):
-        self.postRequests.update(dict)
-        return self.postRequests
+        self.__postRequests.update(dict)
+        return self.__postRequests
 
     def deletePostRequest(self, key):
-        self.postRequests.remove(key)
-        return self.postRequests
+        self.__postRequests.remove(key)
+        return self.__postRequests
 
     def deleteAllPostRequests(self):
-        self.postRequests.clear()
+        self.__postRequests.clear()
 
     def executeGet(self, key):
-        response = requests.get(self.getRequests[key])
+        response = requests.get(self.__getRequests[key])
         self.statusCode = response.status_code
         return response
 
     def executePost(self, key):
-        response = requests.post(self.getRequests[key])
+        response = requests.post(self.__getRequests[key])
         self.statusCode = response.status_code
         return response
 
--- src/SteamworksWrapper.py
+++ src/SteamworksWrapper.py
@@ -2,22 +2,23 @@ import requests
 from src.APIWrapper import APIWrapper
 
 class SteamworksWrapper(APIWrapper):
-    apiKey = None
-    profileId = None
+    __apiKey = None
+    __profileId = None
     #profileRequests = profileRequests()
     #gameAchRequests = self.gameAchRequests()
-    gameAchRequests = {}
+    #__gameAchRequests = {}
 
     gameIds = {"ARK": 376030, "Chivalry 2": 1824220}
-    gameAchString = "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=%d"
+    __gameAchString = "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=%d"
 
     """
-        def profileRequests(self):
-            getProfileString = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.apiKey}={self.profileId}"
-            profileRequests = {self.profileId: getProfileString}
-            return profileRequests
+    def profileRequests(self):
+        getProfileString = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={self.apiKey}={self.profileId}"
+        profileRequests = {self.profileId: getProfileString}
+        return profileRequests
     """
 
+    """
     def gameAchRequests(self):
         gameAchRequests = {}
         for game in self.gameIds:
@@ -25,9 +26,11 @@ class SteamworksWrapper(APIWrapper):
             gameAchRequests.update(game, achRequest)
 
         return gameAchRequests
+        """
+
 
     def getGameAchievments(self, gameId):
-        achRequest = self.gameAchString %gameId
+        achRequest = self.__gameAchString %gameId
 
         response = requests.get(achRequest)
         jsonAch = response.json()
